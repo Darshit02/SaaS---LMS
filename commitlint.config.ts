cadcadcfa5eeda0796f4835442c97f86e5977ebc
Darshit Mistry <darshitmistry2@gmail.com>
2024-09-07 13:11:07 +0530

    build(next-15RC): setup and init deps

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,25 +1,6 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [
-            2,
-            "always",
-            [
-                "feat",
-                "fix",
-                "wip",
-                "patch",
-                "build",
-                "brew",
-                "ci",
-                "chore",
-                "docs",
-                "style",
-                "refactor",
-                "perf",
-                "test",
-                "revert",
-            ],
-        ],
+        "type-enum": [2, "always", ["feat", "fix", "wip", "patch", "build"]],
     },
 }
