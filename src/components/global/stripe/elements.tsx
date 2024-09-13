ac6b3bc10d726d740d6dd1c22dbc8807e6f827f0
Darshit Mistry <darshitmistry2@gmail.com>
2024-09-13 08:40:11 +0530

    feat(group-form) : init slider and create a group

--- src/components/global/stripe/elements.tsx
+++ src/components/global/stripe/elements.tsx
@@ -5,13 +5,12 @@ import { Elements } from "@stripe/react-stripe-js"
 
 type StripeElementsProps = {
   children: React.ReactNode
-  // stripe?: Promise<any>
 }
 
 export const StripeElements = ({ children }: StripeElementsProps) => {
   const { StripePromise } = useStripeElements()
 
   const promise = StripePromise()
-  //@ts-ignore
+
   return promise && <Elements stripe={promise}>{children}</Elements>
 }
